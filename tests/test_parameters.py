030860e7ccca547879bd6d051f830e52ef457879
Hervé BREDIN <hbredin@users.noreply.github.com>
2022-01-26 15:57:06 +0100

    chore: remove tests using ParamList

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -3,7 +3,7 @@
 
 # The MIT License (MIT)
 
-# Copyright (c) 2018-2021 CNRS
+# Copyright (c) 2018-2022 CNRS
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,7 @@
 # Hadrien TITEUX
 import pytest
 
-from pyannote.pipeline.parameter import ParamDict, Uniform, Integer, ParamList, Categorical, Frozen
+from pyannote.pipeline.parameter import ParamDict, Uniform, Integer, Frozen
 from .utils import FakeTrial
 
 
@@ -42,35 +42,6 @@ def test_params_dict():
                                                        'param_b': 0}
 
 
-def test_params_list():
-    params_list = ParamList(*[
-        Integer(i, 10) for i in range(5)
-    ])
-    assert params_list("params_list", FakeTrial()) == list(range(5))
-
-
-def test_nested_params():
-    nested = ParamList(*[
-        ParamDict(**{
-            "param_a": Uniform(0, 1),
-            "param_b": Integer(0, 10)
-        }),
-        Categorical(["a", "b", "c"]),
-        Uniform(0, 100)
-    ])
-    assert nested("nested_params", FakeTrial()) == [{'param_a': 0.0,
-                                                     'param_b': 0},
-                                                    'a', 0.0]
-
-    nested = ParamDict(**{
-        "param_list": ParamList(*[Uniform(i, 10) for i in range(3)]),
-        "param_cat": Categorical(["a", "b", "c"])
-    })
-
-    assert nested("nested_params", FakeTrial()) == {'param_list': [0.0, 1.0, 2.0],
-                                                    'param_cat': 'a'}
-
-
 def test_dict_parameter_freezing():
     param_dict = ParamDict(param_a=Uniform(0, 1),
                            param_b=Integer(5, 10),
@@ -80,18 +51,3 @@ def test_dict_parameter_freezing():
     _, params = zip(*param_dict.flatten().items())
     frozen_params = {param.value for param in params if isinstance(param, Frozen)}
     assert frozen_params == {4, 1}
-
-
-def test_list_parameter_freezing():
-    params_list = ParamList(*[
-        ParamDict(param_a=Uniform(0, 2),
-                  param_b=Uniform(0, 10))
-        for _ in range(5)
-    ])
-
-    with pytest.raises(AssertionError):
-        params_list.freeze([{"param_a": 1, } for _ in range(2)])
-    params_list.freeze([{"param_a": 2} for _ in range(5)])
-    _, params = zip(*params_list.flatten().items())
-    frozen_params = [param.value for param in params if isinstance(param, Frozen)]
-    assert frozen_params == [2] * 5
