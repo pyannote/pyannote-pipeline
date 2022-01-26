0c1dabf02ce102bd9801402b840eb95dffe72dd9
Hervé BREDIN <hbredin@users.noreply.github.com>
2022-01-26 15:58:26 +0100

    chore: remove tests relying on ParamList

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -3,7 +3,7 @@
 
 # The MIT License (MIT)
 
-# Copyright (c) 2018-2021 CNRS
+# Copyright (c) 2018-2022 CNRS
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,7 @@
 # Hadrien TITEUX
 
 from pyannote.pipeline import Pipeline
-from pyannote.pipeline.parameter import Uniform, Integer, ParamDict, ParamList, Categorical
+from pyannote.pipeline.parameter import Uniform, Integer, ParamDict, Categorical
 from .utils import FakeTrial
 
 
@@ -63,15 +63,10 @@ def test_pipeline_params_structured():
                 "param_a": Uniform(0, 1),
                 "param_b": Integer(5, 10)
             })
-            self.params_list = ParamList(*[
-                Integer(i, 10) for i in range(5)
-            ])
 
     pl = TestPipeline()
     fake_params = {'params_dict': {'param_a': 0.0,
-                                   'param_b': 5},
-                   'params_list': [0, 1, 2, 3, 4]}
-
+                                   'param_b': 5}}
     pipeline_tester(pl, fake_params)
 
 
@@ -99,44 +94,3 @@ def test_pipeline_with_subpipeline():
                    }
 
     pipeline_tester(pl, fake_params)
-
-
-def test_pipeline_nested_parameters():
-    class TestPipeline(Pipeline):
-
-        def __init__(self):
-            super().__init__()
-            self.nested_param = ParamList(*[
-                ParamDict(**{
-                    "param_a": Uniform(0, 1),
-                    "param_b": Integer(5, 10)
-                }),
-                Categorical(["a", "b", "c"]),
-                Uniform(50, 100)
-            ])
-
-    pl = TestPipeline()
-    fake_params = {'nested_param': [{'param_a': 0.0, 'param_b': 5}, 'a', 50.0]}
-
-    pipeline_tester(pl, fake_params)
-
-
-def test_pipeline_freezing():
-    class TestPipeline(Pipeline):
-
-        def __init__(self):
-            super().__init__()
-            self.nested_param = ParamList(
-                ParamDict(param_a=Uniform(0, 1),
-                          param_b=Integer(5, 10),
-                          param_c=ParamDict(param_d=Uniform(0, 2),
-                                            param_e=Uniform(0, 10))),
-                Categorical(["a", "b", "c"]),
-                Uniform(50, 100)
-            )
-
-    pl = TestPipeline()
-    frozen_params = {'nested_param': [{'param_b': 7, 'param_c': {'param_d': 2}}, "a", 70]}
-    pl.freeze(frozen_params)
-    pl.instantiate(pl.parameters(FakeTrial()))
-    assert pl.parameters(frozen=True) == frozen_params
