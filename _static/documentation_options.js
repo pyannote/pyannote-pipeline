73eabb6928833ce793eb4ebd2d2af0763e3761c1
hbredin <hbredin@users.noreply.github.com>
2025-02-11 09:58:46 +0000

    deploy: 59af56729dbd045fdc1e868336edde74e9366ca6

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '4.0.0rc2.dev0+g59af567.d20250211',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
